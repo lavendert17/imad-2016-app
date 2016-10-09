ddae874b81211ef89f5b757271e907e2b9c00bba
lavendert17 <lavendert17@gmail.com>
2016-10-09 21:08:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,26 +4,10 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-    
-    
-  app.get('/', function (req, res) {
-  res.send(createTemplate(articleOne));
-});
 
-app.get('/article-one', function ( req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
-app.get('/article-two' , function ( req, res) {
-  
- res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three', function(req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-  
-});
-
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
