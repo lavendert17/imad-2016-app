8776abad84251fcd87465361f474a0666aecc840
lavendert17 <lavendert17@gmail.com>
2016-10-23 05:59:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var content {
+  title:'Article One | Sonia T';
+  heading: 'Article one';
+  date: 'Sep 5, 2016';
+};
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
