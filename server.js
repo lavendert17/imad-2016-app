a2c3fc17152b052f3593d8d6b8f63aa4435a00ad
lavendert17 <lavendert17@gmail.com>
2016-10-25 15:37:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -73,7 +73,6 @@ app.get('/article-three', function (req, res) {
      res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
@@ -85,7 +84,6 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
