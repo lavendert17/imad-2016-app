23faa22cf5bd496236b1434403ab9ce7624a58d2
lavendert17 <lavendert17@gmail.com>
2016-10-26 08:47:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,14 @@ element.innerHTML = ('New Value');
 
 // Move the image
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight () {
+  marginLeft = marginLeft + 10;
+  img.style.marginLeft = marginLeft + 'px';
+  
+}
+}
  img.onclick = function () {
-     var interval = setInterval(moveLeft, 100);
-   img.style.marginLeft = ('100 px');
+     var interval = setInterval(moveRight, 100);
+  
   };
