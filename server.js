8221315b41606ecf742a0dc0fd0a70e260f762e5
lavendert17 <lavendert17@gmail.com>
2016-10-26 10:19:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.get('/', function (req, res) {
 var counter = 0;
 app.get('/counter', function (req, res) {
   counter = counter + 1;
-  res.sendFile(counter.toString());
+  res.send(counter.toString());
 });
 
 app.get('/article-one', function (req, res) {
