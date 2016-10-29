deaecc35a1625bf1fb8c8b1346242d521cff7064
lavendert17 <lavendert17@gmail.com>
2016-10-29 12:55:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,8 +25,5 @@ button.onclick = function(){
        // Not done yet
       
   };
-  //Make the request
-  request.open('GET', 'http://lavendert17.imad.hasura-app.io/counter', true);
-  request.send(null);
   
 };
