f1c7c8c9de23ddd4014d34741cc4a5fdf0ee11d4
lavendert17 <lavendert17@gmail.com>
2016-11-03 10:37:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,11 +16,11 @@ button.onclick = function(){
         if (request.status === 200) {
             //capture a list of names and render it as list.
   
-  var names = ['name1', 'name2', 'name3', 'name4'];
-  var list = '';
-  for (var i=0; i< names.length; i++);{
-    list += '<li>' +names[i] + '</li>';
-  }
+         var names = ['name1', 'name2', 'name3', 'name4'];
+         var list = '';
+         for (var i=0; i< names.length; i++);{
+         list += '<li>' +names[i] + '</li>';
+        }
   var ul = document.getElementById('namelist');
   ul.innerHTML = list;
   
