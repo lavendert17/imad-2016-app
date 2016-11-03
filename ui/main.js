c96d490f0ab9a12e25b322514952099146b9f1e6
lavendert17 <lavendert17@gmail.com>
2016-11-03 11:11:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,35 +16,34 @@ button.onclick = function(){
      // Capture the response and store it in a variable
     request.onreadystatechange = function() {
       if(request.readyState === XMLHttpRequest.DONE) {
-    
-// Take some action
-        
-        if (request.status === 200) {
-            //capture a list of names and render it as list.
+        // Take some action
+          if (request.status === 200) {
+           //capture a list of names and render it as list.
+             var names = request.responseText;
+             names = JSON.parse(names);
+             var list = '';
+             for (var i=0; i< names.length; i++);{
+                  list += '<li>' +names[i] + '</li>';
+            }
+             var ul = document.getElementById('namelist');
+             ul.innerHTML = list;
   
-         var names = request.responseText;
-         names = JSON.parse(names);
-         var list = '';
-         for (var i=0; i< names.length; i++);{
-         list += '<li>' +names[i] + '</li>';
-        }
-        var ul = document.getElementById('namelist');
-        ul.innerHTML = list;
-  
-        }
+          }
     }
        // Not done yet
        
  };
       
-      //Make the request
-      request.open('GET', 'http://lavendert17.imad.hasura-app.io/submit-name?name=' + name, true);
-      request.send(null);
+         //Make a request to the server and send the name
+            var nameInput = document.getElementById('name');
+            var name = nameInput.value;     
+            request.open('GET', 'http://lavendert17.imad.hasura-app.io/submit-name?name=' + name, true);
+            request.send(null);
+      
   
 };
 
- //Make a request to the server and send the name
-  
+     
 
 //capture a list of names and render it as list.
   
@@ -57,7 +56,5 @@ button.onclick = function(){
   ul.innerHTML = list;
   
   };
-var nameInput = document.getElementById('name');
-var name = nameInput.value;     
- 
+
 
