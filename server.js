98cc7eff3f7de6d7b02223bf2619b491abcc5150
lavendert17 <lavendert17@gmail.com>
2016-11-06 16:10:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,25 +5,47 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articleOne = {
-  title: 'Article one | Sonia T',
-  heading: 'Article one',
-  date: 'Sept 5, 2016',
-  content: ` 
-        <p>
-           This is the content for my first article.This is the content for my first article.This is the content for my first article.
-           This is the content for my first article.This is the content for my first article.This is the content for my first article.
-        </p>
-        <p>
-           This is the content for my first article.This is the content for my first article.This is the content for my first article.
-           This is the content for my first article.This is the content for my first article.This is the content for my first article.
-        </p>
-        <p>
-           This is the content for my first article.This is the content for my first article.This is the content for my first article.
-           This is the content for my first article.This is the content for my first article.This is the content for my first article.
-        </p>`
- 
+var articles = {
+      articleOne: {
+      title: 'Article one | Sonia T',
+      heading: 'Article one',
+      date: 'Sept 5, 2016',
+      content: ` 
+            <p>
+               This is the content for my first article.This is the content for my first article.This is the content for my first article.
+               This is the content for my first article.This is the content for my first article.This is the content for my first article.
+            </p>
+            <p>
+               This is the content for my first article.This is the content for my first article.This is the content for my first article.
+               This is the content for my first article.This is the content for my first article.This is the content for my first article.
+            </p>
+            <p>
+               This is the content for my first article.This is the content for my first article.This is the content for my first article.
+               This is the content for my first article.This is the content for my first article.This is the content for my first article.
+            </p>`
+     
+    },
+      articleTwo: { 
+      title: 'Article two | Sonia T',
+      heading: 'Article two',
+      date: 'Sept 10, 2016',
+      content: ` 
+            <p>
+               This is the content for my second article.
+             </p>`
+     
+    },
+      articleThree: {
+      title: 'Article three | Sonia T',
+      heading: 'Article three',
+      date: 'Sept 15, 2016',
+      content: ` 
+            <p>
+               This is the content for my third article.
+             </p>`
+      },
 };
+      
 function createtemplate (data){
         var title = data.title;
         var date = data.date;
@@ -70,8 +92,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
-  res.send(createTemplate(articleOne));
+app.get('/:articleName', function (req, res) {
+    // articleName == article-one
+    // articles[articleName] == {} content object for article one
+    var articleName = req.params.articleName;
+  res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style/css', function (req, res) {
