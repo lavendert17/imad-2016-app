d132dca9828f0847d528fa172f50da4d552c1f93
lavendert17 <lavendert17@gmail.com>
2016-11-06 12:29:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send(Article one requested and will be served here'));
+  res.send('Article one requested and will be served here');
 });
 
 app.get('/ui/style/css', function (req, res) {
@@ -23,5 +23,6 @@ app.get('/ui/madi/png', function (req, res) {
 
 var port = 8080; //Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
-  console.log  
-};
+  console.log  ('IMAD course app listening on port ${port}!');
+  
+});
