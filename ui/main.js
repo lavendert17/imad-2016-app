041e2cd891ca64ea8c9dc5c49a5b6342707e52d5
lavendert17 <lavendert17@gmail.com>
2016-11-06 16:31:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,14 @@
+console.log('Loaded!');
+
+//Change the text of the main text div
+var element = document.getElementById('main-text'
+);
+
+element.innerHTML = 'NEW VALUE';
+
+
+
+
 //counter code
 
 var button = document.getElementById('counter');
