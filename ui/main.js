94a533929627d92b35f98d99b8d67208e8ec893a
lavendert17 <lavendert17@gmail.com>
2016-11-08 20:01:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,16 +28,28 @@ button.onclick = function () {
 };
 //Submit name
 var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
 
-
-     var submit = document.getElementById('submit_btn');
-     submit.onclick = function () {
- 
-     
+    // Create a request object
+    var request = new XMLHttpRequest();
+    
     
 }
-       // Not done yet
-       
+// Capture the response and store in it a variable
+request.onreadystatechange = function () {
+if (request.readyState === XMLHttpRequest.DONE) {
+    // tAKE SOME ACTION
+    if (request.status === 200) {
+       // Capture a list of names and render it as a list
+       var names = ['name1', 'name2', 'name3', 'name4'];
+       var list = '';
+       for (var i=0; i< names.length; i++) {
+           list += '<li>' + names[i] + '<li>';
+        }
+        
+        var ul = document.getElementById
  };
            //Make a request to the server and send the name
             var nameInput = document.getElementById('name');
