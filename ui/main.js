f9d77a8ebbd7b14fb48d372f9a11bbf40ba5a066
lavendert17 <lavendert17@gmail.com>
2016-11-08 19:43:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,24 +26,15 @@ button.onclick = function () {
         request.open('GET', 'http://lavendert17.imad.hasura-app.io/submit-name?name=' + name, true);
         request.send(null);
 };
-    
-        
-     //Submit name
+//Submit name
+var nameInput = document.getElementById('name');
+
 
      var submit = document.getElementById('submit_btn');
      submit.onclick = function () {
  
      
-     //capture a list of names and render it as list.
-     var names = request.responseText;
-     names = JSON.parse(names);
-     var list = '';
-     for (var i=0; i< names.length; i++);{
-          list += '<li>' +names[i] + '</li>';
-    }
-     var ul = document.getElementById('namelist');
-     ul.innerHTML = list;
-    };
+    
 }
        // Not done yet
        
